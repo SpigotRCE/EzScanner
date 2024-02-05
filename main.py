f30072300e31dbe93186e6686e1a2eec53fe0f5b
Milkthedev <128710385+mikthegmr@users.noreply.github.com>
2024-02-05 13:47:54 +0530

    Added a placeholder for the input main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from os import system
 QUBO_COMMAND = 'java -Dfile.encoding=UTF-8 --enable-preview -jar qubo.jar -range {} -ports 26000-28000 -th 8000 -ti 700'
 
 while True:
-    system(QUBO_COMMAND.format(input('')))
+    system(QUBO_COMMAND.format(input('Enter the ip to be scanned')))
